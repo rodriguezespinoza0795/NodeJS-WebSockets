dae55f5e604b348e09b8c186e5c7519b3a77b1da
David Rodriguez <62265080+rodriguezespinoza0795@users.noreply.github.com>
2022-05-19 23:59:10 -0500

    Query and body

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 - Install dependences: `npm install`
 - Run server AutoReload: `npm run dev`
 
-List Items Method: GET `http://localhost:3000/message`
+List Items Method: GET `http://localhost:3000/message?orderBy=id&age=26`  
 Add New item Method: POST `http://localhost:3000/message`
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express')
 
-var app = express();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.get('/message', (req, res) => {
     res.send('Lista de Mensajes')
+    console.log('BODY', req.body)
+    console.log('QUERY', req.query)
 })
 
 app.post('/message', (req, res) => {
     res.send('Mensaje añadido')
 })
 
-app.listen('3000')
-
-console.log('La applicación está escuchando en http://localhost:3000')
+app.listen(PORT, () => {
+    console.log(`La applicación está escuchando en http://localhost:${PORT}`)
+});
